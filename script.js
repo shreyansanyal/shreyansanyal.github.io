d464c95971a54a46a9364749bbf2cc1f46dfc774
Shreyan <shreyansanyal@gmail.com>
2025-02-22 03:36:02 +0530

    New UI.

---
+++ script.js
@@ -0,0 +1,19 @@
+// Typewriter Effect
+const typewriterText = "Hi, I'm Shreyan!";
+const typewriterElement = document.getElementById("typewriter");
+let i = 0;
+
+function typeWriter() {
+  if (i < typewriterText.length) {
+    typewriterElement.innerHTML += typewriterText.charAt(i);
+    i++;
+    setTimeout(typeWriter, 300);
+  }
+}
+
+typeWriter();
+
+// Particles.js Background
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('Particles.js loaded');
+});
